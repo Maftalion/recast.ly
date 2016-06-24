1ed37acb52dba984f7d9bfe00c1e1e65f3bc51ba
maftalion <aftalion.matt@gmail.com>
2016-06-24 16:11:45 -0700

    Populate video list dynamically while staying stateless

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,6 +1,6 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list media">
-    <VideoListEntry />
+    {props.videos.map(video => <VideoListEntry video={video} />)}
   </div>
 );
 
