1b0b4af422bcb62d85e7b466bb5a1a2322b186aa
maftalion <aftalion.matt@gmail.com>
2016-06-24 17:31:28 -0700

    Begin organizing click handler code to play other videos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,29 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer video={window.exampleVideoData[0]}/>
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={window.exampleVideoData} />
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      currVid: this.props.data[0],
+      clickVLE: function(event) {
+        console.log(event);
+      }
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.currVid} state={this.state} />
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={this.props.data} state={this.state} />
+        </div>
+      </div>
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
-window.App = App;
+window.App = App; //(window.exampleVideoData);
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,13 +1,13 @@
 var VideoList = (props) => (
   <div className="video-list media">
-    {props.videos.map(video => <VideoListEntry video={video} />)}
+    {props.videos.map(video => <VideoListEntry video={video} state={props.state} />)}
   </div>
 );
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
-  videos: React.PropTypes.array.isRequired
+  videos: React.PropTypes.array.isRequired,
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
