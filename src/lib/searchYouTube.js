9360628648361418f446ce722c5058bcffedd986
maftalion <aftalion.matt@gmail.com>
2016-06-24 22:03:21 -0700

    Properly grab data from YouTube

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,11 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  var videos = $.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=' + 
+                      options.max + 
+                      '&q=' + 
+                      options.query + 
+                      '&type=video&videoEmbeddable=true&key=' + 
+                      options.key,
+                      function(data) { callback(data.items); } );
 };
 
 window.searchYouTube = searchYouTube;
